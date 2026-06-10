3b465bc6c115e2cae30f63f60a0f28072725da1d
YasenYanev <yasenyanev311@gmail.com>
2026-06-10 21:04:21 +0300

    fix: prevent render aborts for unsupported weather conditions

--- src/modules/icon-name.js
+++ src/modules/icon-name.js
@@ -5,6 +5,11 @@ export default function getIconName(weatherCondition, time) {
   const weatherIconPair = weatherConditions.find(
     obj => obj.condition === weatherCondition
   );
+
+  if (!weatherIconPair) {
+    return time > '06:00' && time < '20:00' ? 'wi-day-cloudy' : 'wi-night-alt-cloudy';
+  }
+
   return time > '06:00' && time < '20:00'
     ? weatherIconPair.dayIcon
     : weatherIconPair.nightIcon;
--- src/modules/moon-phase-icon.js
+++ src/modules/moon-phase-icon.js
@@ -2,5 +2,10 @@ import moonPhases from '../assets/data/moon_phases.json';
 
 export default function getMoonPhaseIcon(moonPhase) {
   const moonIconPair = moonPhases.find(obj => obj.moonPhase === moonPhase);
+
+  if (!moonIconPair) {
+    return 'wi-moon-alt-new';
+  }
+
   return moonIconPair.icon;
 }
